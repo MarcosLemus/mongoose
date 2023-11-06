fd05a14f16d6f3f84bfd28e61ce0168ad5920aed
MarcosLemus <marcoslemus1990@gmail.com>
2023-11-06 13:45:37 +0100

    practica del viernes

---
+++ README.md
@@ -0,0 +1,8 @@
+# CRUD
+
+## Implementa las funciones para crear un CRUD funcional usando el modelo de Mongoose Book
+
+- [] Instala las dependencias `npm install`.
+- [] Ejecuta las pruebas `npm test`.
+- [] Lee atentamente los errores que tiran las pruebas y modifica `crud.js` para las pasen.
+- [] Si fuese necesario inspecciona `__test__/crud.test.js` para buscar pistas.
---
+++ src/crud.js
@@ -0,0 +1,24 @@
+const Book = require("./book");
+
+const book = Book;
+
+const getBookById = (id) => {
+  if (book.id === book.id) {
+  }
+};
+
+const getAllBooks = () => {};
+
+const createBook = (bookDetails) => {};
+
+const removeBookById = (id) => {};
+
+const updateBookById = (id, update) => {};
+
+module.exports = {
+  getBookById,
+  getAllBooks,
+  createBook,
+  removeBookById,
+  updateBookById,
+};
